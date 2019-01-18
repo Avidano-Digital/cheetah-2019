ddee48d16d8fd500b3acdd02652f67ad3dbe23dd
Carl Avidano <cavidano@gmail.com>
2019-01-18 04:03:13 -0500

    updating theme

--- wp-content/themes/ccf-twentynineteen/include/primary-navigation.php
+++ wp-content/themes/ccf-twentynineteen/include/primary-navigation.php
@@ -1,14 +1,14 @@
-<ul class="no-reverse">
+<ul class="extensible-list horizontal">
     <li>
         <a href="#1" title="About Us">About Us</a>
         
-        <ul class="sub">
+        <ul class="extensible-list sub">
             <li>
                 <div class="row no-gutters" style="width:480px;">
 
                     <div class="col-6">
  
-                        <ul>
+                        <ul class="extensible-list">
                             <li class="leader">What We Do</li>
                             <li><a href="/about-us/what-we-do/research/">Research</a></li>
                             <li><a href="/about-us/what-we-do/education/">Education</a></li>
@@ -22,7 +22,7 @@
 
                     <div class="col-6">
 
-                        <ul>
+                        <ul class="extensible-list">
                             <li class="leader">Who We Are</li>
                             <li><a href="/about-us/who-we-are/mission-and-vision/">Mission and Vision</a></li>
                             <li><a href="/about-us/who-we-are/dr-laurie-marker/">Dr. Laurie Marker</a></li>
@@ -42,7 +42,7 @@
     </li>
     <li>
         <a href="#1" title="Get Involved">Get Involved</a>
-        <ul class="sub">
+        <ul class="extensible-list sub">
             <li><a href="#1">Ways to Give</a></li>
             <li><a href="#1">Events</a></li>
             <li><a href="#1">Volunteer Locally</a></li>
@@ -53,7 +53,7 @@
     </li>
     <li>
         <a href="#1" title="Learn ">Learn</a>
-        <ul class="sub">
+        <ul class="extensible-list sub">
             <li><a href="/about-us/learn/resource-library/">Resource Library</a></li>
             <li><a href="#1">About Cheetahs</a></li>
             <li><a href="#1">Human Wildlife Conflict</a></li>
@@ -64,7 +64,7 @@
     </li>
     <li> 
         <a <?php if($inside_news !== false ) : ?> class="active" <?php endif; ?> href="#1" title="News">News</a>
-        <ul class="sub">
+        <ul class="extensible-list sub">
             <li><a href="/ccf-blog">CCF Blog</a></li>
             <li><a href="#1">Press Releases</a></li>
             <li><a href="#1">Press Kit</a></li>
