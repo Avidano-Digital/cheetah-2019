b0550ea1c4b165cebd08dad41069eaea426374e7
Sean Dennis <sean@fullwindsor.co>
2019-03-14 06:56:43 -0500

    Made related posts display conditional.

--- wp-content/themes/ccf-twentynineteen/template-parts/related-reading.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/related-reading.php
@@ -1,13 +1,3 @@
-<hr class="mx-n2">
-
-<div class="container-fluid">
-
-    <div class="narrow py-6">
-
-        <p class="h5 mb-3">Related Reading</p>
-
-        <ul class="list-group list-group-flush">
-
 <?php
             $relatedTag = get_the_title();
             $args = array( 
@@ -18,45 +8,61 @@
 
             $relatedPosts = new WP_Query( $args );
 
-            while ( $relatedPosts->have_posts() ) : $relatedPosts->the_post();
+            if ( $relatedPosts->have_posts() ):
+?>
+                <hr class="mx-n2">
 
-                $featured_image_id = get_post_thumbnail_id();
-                $featured_image = wp_get_attachment_image_src($featured_image_id,'thumbnail', false, '');
-                $featured_image_alt = get_post_meta($featured_image_id,'_wp_attachment_image_alt', true);
+                <div class="container-fluid">
+
+                    <div class="narrow py-6">
+
+                        <p class="h5 mb-3">Related Reading</p>
+
+                        <ul class="list-group list-group-flush">
+
+<?php
+                            while ( $relatedPosts->have_posts() ) : $relatedPosts->the_post();
+
+                                $featured_image_id = get_post_thumbnail_id();
+                                $featured_image = wp_get_attachment_image_src($featured_image_id,'thumbnail', false, '');
+                                $featured_image_alt = get_post_meta($featured_image_id,'_wp_attachment_image_alt', true);
 ?>
 
-                <li class="list-group-item">
+                                <li class="list-group-item">
 
-                    <div class="row align-items-center">
+                                    <div class="row align-items-center">
 
-                        <div class="col">
-                            <p class="f-sans-serif fs-md mb-0"><?php echo get_the_date(); ?></p>
-                            <a href="<?php the_permalink(); ?>">
-                                <strong>
-                                    <?php the_title(); ?>
-                                </strong>
-                            </a>
-                        </div>
-                        <!-- .col -->
+                                        <div class="col">
+                                            <p class="f-sans-serif fs-md mb-0"><?php echo get_the_date(); ?></p>
+                                            <a href="<?php the_permalink(); ?>">
+                                                <strong>
+                                                    <?php the_title(); ?>
+                                                </strong>
+                                            </a>
+                                        </div>
+                                        <!-- .col -->
 
-                        <div class="col-auto">
-                            <img src="<?php echo $featured_image[0]; ?>" alt="<?php echo $featured_image_alt; ?>" style="max-width:150px;">
-                        </div>
-                        <!-- .col -->
+                                        <div class="col-auto">
+                                            <img src="<?php echo $featured_image[0]; ?>" alt="<?php echo $featured_image_alt; ?>" style="max-width:150px;">
+                                        </div>
+                                        <!-- .col -->
 
-                    </div>
-                    <!-- .row -->
+                                    </div>
+                                    <!-- .row -->
 
-                </li>
+                                </li>
 <?php
-            endwhile;
+                            endwhile;
 ?>   
 
-        </ul>
+                        </ul>
 
 
-    </div>
-    <!-- .narrow -->
+                    </div>
+                    <!-- .narrow -->
 
-</div>
-<!-- .container-fluid -->
+                </div>
+                <!-- .container-fluid -->
+<?php
+            endif;
+?>                
