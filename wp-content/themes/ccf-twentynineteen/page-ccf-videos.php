8bf44bb219705315733c849f21aecda7170389f2
Sean Dennis <sean@fullwindsor.co>
2019-04-02 17:06:44 -0500

    Removed banner from CCF Videos template.

--- wp-content/themes/ccf-twentynineteen/page-ccf-videos.php
+++ wp-content/themes/ccf-twentynineteen/page-ccf-videos.php
@@ -10,39 +10,6 @@ get_header(); ?>
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <section class="banner">
-
-        <div class="card bg-white">
-
-            <div class="overlay-gradient-y-black">
-        
-<?php 
-                $featured_image_id = get_post_thumbnail_id($post->ID);
-                $featured_image = wp_get_attachment_image_src($featured_image_id,'full', false, '');
-                $featured_image_alt = get_post_meta($featured_image_id,'_wp_attachment_image_alt', true);
-?>
- 	
-            <?php if( $featured_image ): ?>
-                <img class="card-img" src="<?php echo $featured_image[0]; ?>" alt="<?php echo $featured_image_alt; ?>">
-            <?php else : ?>
-                <img class="card-img" src="http://via.placeholder.com/1500x500.jpg" alt="Placeholder">
-            <?php endif; ?>
-
-            </div>
-
-            <div class="card-img-overlay d-flex">
-                <div class="container-fluid align-self-end opacity-70">
-                    <h1 class="text-right text-secondary">
-                        <em>Learn</em>
-                    </h1>
-                </div>
-                <!-- .align-self-center -->
-            </div>
-        </div>
-
-    </section>
-    <!-- .banner -->
-
     <article class="container-fluid">
 
         <div class="row">
