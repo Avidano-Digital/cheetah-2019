718f7cbcebda08305309224974c44543c0dd207d
Carl Avidano <cavidano@gmail.com>
2019-04-04 06:38:54 -0400

    updated modals

--- wp-content/themes/ccf-twentynineteen/template-parts/ccf-cheetah-modals.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/ccf-cheetah-modals.php
@@ -159,3 +159,90 @@
 
 </div>
 <!-- .modal -->
+
+<div class="modal fade" id="cheetah-bella" tabindex="-1" role="dialog" aria-labelledby="cheetah-bella-title"
+    aria-hidden="true">
+
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+
+        <div class="modal-content">
+
+            <div class="modal-header border-bottom-0">
+                <div class="row no-gutters w-100 align-items-center">
+                    <div class="col">
+                        <h1 class="modal-title h4 mb-0" id="cheetah-bella-title">Bella</h1>
+                    </div>
+                    <div class="col-auto">
+                        <button class="no-btn-style" type="button" data-dismiss="modal" aria-label="Close">
+                            <span class="fas fa-times"></span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- .modal-header -->
+
+            <div class="modal-body p-0">
+
+                <img src="<?php echo get_template_directory_uri(); ?>/images/sponsor/bella.jpg" alt="Placeholder">
+
+                <div class="px-2 py-4 fs-md">
+                    <p>
+                        Bella came to CCF in December 2017 and is about a year and a half old. She was kept as a pet on
+                        a private farm
+                        with a male cheetah, Katiti. After not having permits granted to continue keeping them from the
+                        Ministry of
+                        Environment and Tourism, the pair were handed over to CCF.
+                    </p>
+                    <p>
+                        Before arriving at CCF, the pair had a poor diet of chicken necks so have been transferred on to
+                        a diet of red
+                        meat with a vitamin and calcuim supplement to help them grow and develop properly. Bella is very
+                        habituated to
+                        people and will always purr whenever people are around her.
+                    </p>
+                    <p>
+                        She is confident in nature and is learning quickly during her training sessions at CCF. Both
+                        Bella and Katiti
+                        are inseparable, spending most of their days together exploring the new enclosures and meeting
+                        other cheetahs.
+                    </p>
+                </div>
+                <!-- .p-2 -->
+
+            </div>
+            <!-- .modal-body -->
+
+            <div class="modal-footer">
+
+                <div class="row matrix-gutter align-items-center">
+                    <div class="col-auto">
+                        <button class="no-btn-style btn-prev" type="button">
+                            <span class="fas fa-arrow-left" title="Previous" role="img"></span>
+                        </button>
+                    </div>
+                    <!-- .col -->
+                    <div class="col">
+                        <a href="#1" class="btn btn-block btn-primary">Sponsor</a>
+                    </div>
+                    <!-- .col -->
+                    <div class="col-auto">
+                        <button class="no-btn-style btn-next" type="button">
+                            <span class="fas fa-arrow-right" title="Next" role="img"></span>
+                        </button>
+                    </div>
+                    <!-- .col -->
+                </div>
+                <!-- .row -->
+
+            </div>
+            <!-- .modal-footer -->
+
+        </div>
+        <!-- .modal-content -->
+
+    </div>
+    <!-- .modal-dialog -->
+
+</div>
+<!-- .modal -->
