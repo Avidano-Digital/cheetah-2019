373b41633c94c26059841a5619f40455e2e37ce4
Carl Avidano <cavidano@gmail.com>
2019-04-09 04:01:29 -0400

    adding placeholder content

--- wp-content/themes/ccf-twentynineteen/page-kids.php
+++ wp-content/themes/ccf-twentynineteen/page-kids.php
@@ -47,7 +47,40 @@ $parent_title = get_the_title($post->post_parent);
 
     <section class="container-fluid wide overflow-hidden p-0">
 
-        <div class="featured-panel borderX responsive-lg my-5">
+        <div class="container my-6">
+        
+            <div class="row align-items-center">
+                <div class="col-lg-6 mb-3 mb-lg-0">
+                    <div class="narrow rounded-lg overflow-hidden">
+
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NpEaa2P7qZI" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
+                        </div>
+
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+                <div class="col-lg-6">
+                    <div class="narrow">
+                        <p>
+                            Unlike other “big cats”, a classification that includes: lions, tigers, leopards, jaguars and cougars)
+                            cheetahs don’t roar. They growl when facing danger, and they vocalize with sounds more equivalent to a
+                            high-pitched chirp or bubble and they bark when communicating with each other. The cheetah is also
+                            unique among big cats in that it can also purr while both inhaling and exhaling.
+                        </p>            
+                    </div>
+                </div>
+                <!-- .col -->
+            
+            </div>
+            <!-- .row -->
+
+        </div>
+        <!-- .container -->
+
+        <div class="featured-panel responsive-lg my-6">
 
             <div class="card bg-white">
 
@@ -96,7 +129,7 @@ $parent_title = get_the_title($post->post_parent);
 
         </div>
 
-        <div class="featured-panel responsive-lg my-5">
+        <div class="featured-panel responsive-lg my-6">
 
             <div class="card bg-light">
                 <div class="overlay-gradient-y-white d-none d-lg-block">
@@ -130,7 +163,113 @@ $parent_title = get_the_title($post->post_parent);
 
         </div>
 
-        <div class="featured-panel wide borderX responsive-lg my-5">
+        <div class="my-6">
+
+            <div class="row matrix-gutter">
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-end">
+                                <div class="narrow text-center my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        Tail like a rudder
+                                    </h2>
+                                    <p>
+                                        The cheetah has a long, muscular tail that has a flat shape. The tail almost functions
+                                        like a rudder on a boat because they use it to help control their steering and keep
+                                        their balance when running very fast.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-end">
+                                <div class="narrow text-center my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        Special cleat feet
+                                    </h2>
+                                    <p>
+                                        The cheetah has “semi non-retractable” claws (almost like dog claws) that work like
+                                        the cleats on a football shoe to give the cheetah a lot of traction when running.
+                                        The pads of most cats’ paws are soft, but the cheetah’s pads are hard kind of like
+                                        the rubber on a tire. This also helps them grip the ground when they are running so
+                                        fast.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-end">
+                                <div class="narrow my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        Tear marks
+                                    </h2>
+                                    <p>
+                                        Like football players, cheetahs have built in sun glare protection around their
+                                        eyes. The cheetah’s “tear marks” run from the inside corners of their eyes down to
+                                        the outside edges of their mouth. These marks help reflect the glare of the sun when
+                                        they are hunting during the day. They work just like the black marks that football
+                                        players put under their eyes during the games. These marks also work like the sights
+                                        on a rifle, to help the cheetah “aim” and stay focused on their prey when they are
+                                        hunting.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-end">
+                                <div class="narrow my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        Spotted skin
+                                    </h2>
+                                    <p>
+                                        The cheetah’s fur is covered in solid black spots, and so is their skin! The black
+                                        fur actually grows out of the black spots on their skin.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+            
+            </div>
+        
+        </div>
+
+        <div class="featured-panel responsive-lg my-6">
 
             <div class="card bg-white">
             
@@ -194,9 +333,63 @@ $parent_title = get_the_title($post->post_parent);
 
         </div>
 
+        <div class="my-6">
+
+            <div class="row matrix-gutter">
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-center">
+                                <div class="narrow text-center my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        Unique among cats
+                                    </h2>
+                                    <p>
+                                        The cheetah has a long, muscular tail that has a flat shape. The tail almost functions
+                                        like a rudder on a boat because they use it to help control their steering and keep
+                                        their balance when running very fast.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+                <div class="col-lg-6">
+
+                    <div class="card bg-primary">
+                        <img class="card-img show-on-mobile" src="https://via.placeholder.com/1200x1200" alt="Card image">
+                        <div class="card-img-overlay d-flex">
+                            <div class="container align-self-center">
+                                <div class="narrow text-center my-3">
+                                    <h2 class="card-title display-3 mb-2 f-cheetah-tracks text-tertiary">
+                                        A cheetah’s favorite food
+                                    </h2>
+                                    <p>
+                                        Cheetahs are carnivores, and feed mostly on smaller antelope like springbok,
+                                        steenbok, Thomson’s gazelle, and duiker. They usually chase down their prey and
+                                        then bite its throat, killing it by cutting off its air supply (suffocation).
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .col -->
+
+            
+            </div>
+        
+        </div>
+
     </section>
     <!-- .container-fluid -->
-
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php endwhile; endif; /* have_posts */ ?>
