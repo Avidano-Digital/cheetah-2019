0c2e25d9ac8d3168971eabbec2ad2a42a405b5ba
Sean Dennis <sean@fullwindsor.co>
2019-05-02 10:21:45 -0500

    Added fields for kid artists of the month.

--- wp-content/themes/ccf-twentynineteen/template-parts/kid-artists.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/kid-artists.php
@@ -82,125 +82,47 @@
 
     <div class="narrow gallery-thumbnails my-5">
 
-    <div class="artist-bio my-3 text-center">
-    <h3 class="text-tertiary"><?php the_title(); ?></h3>
-    <p><?php the_field('description'); ?></p>
+        <div class="artist-bio my-3 text-center">
+            <h3 class="text-tertiary"><?php the_title(); ?></h3>
+            <p><?php the_field('description'); ?></p>
     
-    </div>
-    <!-- .artist-bio -->
-
-        <div class="row matrix-gutter">
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-sm-6">
-
-                <div class="position-relative">
-
-                    <a class="enlarge" class="stretched-link" href="https://placehold.it/1200x800.jpg"
-                        title="A caption for the image." data-toggle="lightbox"
-                        data-gallery="example-gallery" data-footer="A caption for the image.">
-                        <span>
-                            <span class="fas fa-expand"></span>
-                        </span>
-                    </a>
-
-                    <img src="https://placehold.it/1200x800.jpg" alt="A caption for the image.">
-                </div>
-
-            </div>
-            <!-- .col -->
-
         </div>
-        <!-- .row -->
+        <!-- .artist-bio -->
+
+        <?php 
+
+        $images = get_field('gallery');
+
+        if ($images) : 
+
+        ?>
+
+            <div class="row matrix-gutter">
+
+                <?php foreach( $images as $image ): ?>
+                    <div class="col-sm-6">
+
+                        <div class="position-relative">
+
+                            <a class="enlarge" class="stretched-link" href="<?php echo $image['url']; ?>"
+                                title="<?php echo $image['alt']; ?>" data-toggle="lightbox"
+                                data-gallery="example-gallery" data-footer="<?php echo $image['alt']; ?>">
+                                <span>
+                                    <span class="fas fa-expand"></span>
+                                </span>
+                            </a>
+
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                        </div>
+
+                    </div>
+                    <!-- .col -->
+                <?php endforeach; ?>
+
+            </div>
+            <!-- .row -->
+
+        <?php endif; ?>
 
     </div>
     <!-- .gallery-thumbnails -->
