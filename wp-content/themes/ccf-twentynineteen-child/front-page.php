da3abfa5930e70607d9f04ff620280c7b14612b2
Carl Avidano <cavidano@gmail.com>
2019-05-08 03:03:41 -0400

    updates

--- wp-content/themes/ccf-twentynineteen-child/front-page.php
+++ wp-content/themes/ccf-twentynineteen-child/front-page.php
@@ -26,9 +26,9 @@
           </div>
 
           <div class="card-img-overlay d-flex">
-            <div class="align-self-end w-100">
-              <div class="medium text-white text-center py-4">
-                <h1 class="card-title display-4 mb-3 text-shadow">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+            <div class="align-self-end">
+              <div class="medium text-white text-center my-lg-3">
+                <h1 class="card-title display-4 mb-2 text-shadow">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
                 <a href="#" class="btn btn-lg btn-primary">Our Mission</a>
               </div>
             </div>
@@ -47,32 +47,41 @@
 
       <div class="container">
 
-        <h2 class="sr-only">Cheetah Conservation Fund Canada Overview</h2>
+        <div class="medium mb-4">
 
-        <div class="row mb-5">
-          <div class="col-md-8 offset-md-2">
-            <div class="embed-responsive embed-responsive-16by9 shadow border">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0hyNVlYVTNo" frameborder="0" allowTransparency="true"
-                allowfullscreen="true"></iframe>
+            <div class="rounded-lg overflow-hidden mb-4">
+            
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0hyNVlYVTNo" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
+                </div>
+
             </div>
-          </div>
+            <!-- .rounded-lg -->
         </div>
+        <!-- .narrow -->
 
-        <div class="row">
-          <div class="col-md mb-3 mb-md-0">
-            <p>We fundraise for a highly innovative wildlife conservation organization in Namibia, Cheetah Conservation Fund,
-              whose primary goal is to save cheetahs from extinction. We hold fundraising events and educate Canadians. We
-              are a registered charity run by Canadian volunteers – we direct 98% of all funds to charitable programming
-              in Namibia.</p>
-          </div>
-          <div class="col-md">
-            <p>Since 2013, we have provided close to $310,000; and our impact has been in the support of school outreach, education
-              sessions with Future Farmers of Africa, the support of the livestock guarding dogs, and the care of cheetahs.
-              Our goal is to save cheetahs in the wild and help ensure that local communities can prosper in harmony with
-              wildlife and protect their shared ecosystems.</p>
-          </div>
+        <div class="medium">
+
+            <div class="row matrix-gutter">
+                <div class="col-md-6">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consequuntur perspiciatis ab? Saepe incidunt doloribus eligendi quas sequi odit.
+                    </p>
+                </div>
+                <!-- .col -->
+                <div class="col-md-6">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae neque quasi omnis dolores optio.
+                    </p>
+                </div>
+                <!-- .col -->
+            
+            </div>
+            <!-- .row -->
+        
         </div>
-        <!-- .row -->
+        <!-- .medium -->
+
 
       </div>
       <!-- .container -->
@@ -81,7 +90,148 @@
 
     <!-- #introduction -->
 
-    <section class="my-6">
+    <section class="py-5 py-xl-7">
+
+      <h2 class="sr-only">News and Videos from CCF</h2>
+
+      <section class="container mb-5" id="latest-news">
+
+        <header class="text-center mb-4">
+          <h3 class="display-4 mb-0">Latest News</h3>
+          <a class="blended-link fs-md" href="#">All News</a>
+        </header>
+
+        <div class="row matrix-border">
+
+          <?php
+
+          global $post;
+          
+          $args = array( 'category_name' => 'conservation', 'posts_per_page' => 3, 'orderby' => 'date' );
+          $postslist = get_posts( $args );
+          
+          foreach ( $postslist as $post ) :
+          setup_postdata( $post );
+          
+          $the_date = get_the_date();
+          
+          ?>
+
+          <div class="col-lg-4 mb-5 mb-lg-0">
+          
+              <a class="featured-article" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+              
+                  <div>
+
+                      <span><?php echo $the_date; ?></span>
+
+                      <?php if( has_post_thumbnail() ):
+                          $featured_image_url = get_the_post_thumbnail_url( get_the_ID(),'full' );
+                      ?>
+                                      
+                      <img class="w-100" src="<?php echo $featured_image_url; ?>" alt="<?php the_title(); ?>">
+
+                      <?php else : ?>
+                      <img class="w-100" src="https://via.placeholder.com/1000x563" alt="Placeholder">
+
+
+                      
+                      <?php endif; /* featured_image */ ?>
+
+                  </div>
+
+                  <p class="h5"><?php the_title(); ?></p>
+
+              </a>
+
+          </div>
+          <!-- .col -->
+
+          <?php endforeach; wp_reset_postdata(); ?>
+
+        </div>
+        <!-- .row -->
+
+      </section>
+      <!-- #latest-news -->
+
+      <section class="container-fluid wide" id="events">
+
+        <header class="text-center mb-4">
+            <h3 class="display-4 mb-0">Upcoming Events</h3>
+            <a class="blended-link fs-md" href="#">All Events</a>
+        </header>
+
+        <div class="row matrix-border">
+
+          <div class="col-md-6 d-md-flex mb-3 mb-md-0 col-xl-4 offset-xl-2">
+            <div class="card bg-light">
+              <div class="card-header bg-info text-white text-center font-weight-bold py-2">
+                September 29th, 2018
+              </div>
+              <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/events/02.jpg" alt="Placeholder">
+              <div class="card-body">
+                <h4 class="card-title mb-2">Discover Namibia</h4>
+                <p class="card-text fs-md">Join us for wine & cheese while taking a virtual tour of Namibia with professional
+                  photographer and travel consultant Vanessa Dewson.</p>
+              </div>
+              <div class="card-footer pt-0">
+                <a class="btn btn-primary btn-block" href="#">Event Details</a>
+              </div>
+            </div>
+          </div>
+          <!-- .col -->
+
+          <div class="col-md-6 d-md-flex col-xl-4">
+            <div class="card bg-light">
+              <div class="card-header bg-info text-white text-center font-weight-bold py-2">
+                October 26th, 2018
+              </div>
+              <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/events/01.jpg" alt="Placeholder">
+              <div class="card-body">
+                <h4 class="card-title mb-2">Dr. Laurie Marker Returns to Ottawa, Canada</h4>
+                <p class="card-text fs-md">We are pleased to announce that Dr. Laurie Marker, founder and executive director of
+                  Cheetah Conservation Fund (CCF),
+                  will be coming back to Canada this fall.</p>
+              </div>
+              <div class="card-footer pt-0">
+                <a class="btn btn-primary btn-block" href="#">Event Details</a>
+              </div>
+            </div>
+          </div>
+          <!-- .col -->
+
+
+        </div>
+        <!-- .matrix-border -->
+
+      </section>
+      <!-- #videos -->
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section class="my-6 d-none">
       
       <div class="container my-5" id="latest-news">
 
