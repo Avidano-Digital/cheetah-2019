94ee40a9ff55fe2a6a1951631dc1426836a754c9
Carl Avidano <cavidano@gmail.com>
2019-05-10 13:38:34 -0400

    scientif author starting point

--- wp-content/themes/ccf-twentynineteen/include/primary-navigation.php
+++ wp-content/themes/ccf-twentynineteen/include/primary-navigation.php
@@ -1,6 +1,8 @@
 <ul class="extensible-list horizontal justify-content-end">
 
     <?php starterMenu('Desktop'); ?>
+
+    <!-- Sean's new menu -->
     
     <li>
         <a class="btn btn-primary donate" href="/donate" title="Donate">Donate</a>
