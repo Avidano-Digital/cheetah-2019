e1824537a86af9fb1a419bcde376319765c9d154
Sean Dennis <sean@fullwindsor.co>
2019-05-11 08:46:40 -0500

    Added hook to change news author meta on post save.

--- wp-content/themes/ccf-twentynineteen/lib/acf-theme-options.php
+++ wp-content/themes/ccf-twentynineteen/lib/acf-theme-options.php
@@ -13,3 +13,31 @@
         );
         
     }
+
+    function change_news_author( $post_id ) {
+       
+        if ( empty($_POST['acf']) ) :
+            return;
+        endif;
+        
+        // get term id from $post_id
+        $stored_author = wp_get_post_terms($post_id, 'news-author');
+
+        if (get_post_type() == 'post') :
+
+            $stored_author_id = $stored_author[0]->term_id;
+            
+            // get submitted value from acf form
+            $posted_author = $_POST['acf']['field_5cd5d3f9cf88b'];
+            
+            // get term_id for the submitted value       
+            $term_id = get_term_by( 'id', $posted_author, 'news-author' );
+            
+            // if stored value is not equal to posted value, then update terms
+            if ($stored_author_id != $posted_author) :
+                wp_set_object_terms( $post_id, $term_id->term_id, 'news-author' );
+            endif;
+
+        endif;
+    }
+    add_action('acf/save_post', 'change_news_author', 20);
