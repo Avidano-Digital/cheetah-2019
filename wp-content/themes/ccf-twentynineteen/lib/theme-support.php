e92eb3920b81b6eabeca31efc7e526589cd83820
Carl Avidano <cavidano@gmail.com>
2019-05-12 03:58:47 -0400

    cleaned up theme support

--- wp-content/themes/ccf-twentynineteen/lib/theme-support.php
+++ wp-content/themes/ccf-twentynineteen/lib/theme-support.php
@@ -190,16 +190,9 @@
 
     add_action( 'init', 'create_parent_post_types' );
 
-
-
-
-
-
-
-
-
-
-
+    ////////////////////////////////////////
+    // Change 'Posts' to 'News'
+    ////////////////////////////////////////
 
     function customize_post_object() {
 
@@ -373,7 +366,6 @@
         echo '<ul class="extensible-list fs-md">';
 
         if ($current_category_name !== 'Press Releases' && $current_category_name !== 'Cheetah Strides') :
-
 
         foreach ($topics as $topic) :
 
@@ -636,73 +628,80 @@
     add_shortcode( 'cheetahs', 'cheetahs' );
 
 
-// Numbered Pagination
+    ////////////////////////////////////////
+    // Numbered Pagination
+    ////////////////////////////////////////
 
-if (!function_exists('custom_pagination')) {
 
-    function custom_pagination(){
+    if (!function_exists('custom_pagination')) {
 
-        $prev_arrow = is_rtl() ? '→' : '←';
-        $next_arrow = is_rtl() ? '←' : '→';
-        
-        global $wp_query;
-        $total = $wp_query->max_num_pages;
-        
-        $big = 999999999; // need an unlikely integer
-        
-        if ($total > 1) {
+        function custom_pagination(){
 
-            if (!$current_page = get_query_var('paged')) {
-                $current_page = 1;
-            }
+            $prev_arrow = is_rtl() ? '→' : '←';
+            $next_arrow = is_rtl() ? '←' : '→';
             
-            if (get_option('permalink_structure')) {
-                $format = 'page/%#%/';
-            } else {
-                $format = '&paged=%#%';
+            global $wp_query;
+            $total = $wp_query->max_num_pages;
+            
+            $big = 999999999; // need an unlikely integer
+            
+            if ($total > 1) {
+
+                if (!$current_page = get_query_var('paged')) {
+                    $current_page = 1;
+                }
+                
+                if (get_option('permalink_structure')) {
+                    $format = 'page/%#%/';
+                } else {
+                    $format = '&paged=%#%';
+                }
+
+                echo paginate_links(array(
+                    'base'			=> str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                    'format'		=> $format,
+                    'current'		=> max(1, get_query_var('paged')),
+                    'total' 		=> $total,
+                    'mid_size'		=> 3,
+                    'type' 			=> 'plain',
+                    'prev_text'		=> $prev_arrow,
+                    'next_text'		=> $next_arrow,
+                ));
             }
-
-            echo paginate_links(array(
-                'base'			=> str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-                'format'		=> $format,
-                'current'		=> max(1, get_query_var('paged')),
-                'total' 		=> $total,
-                'mid_size'		=> 3,
-                'type' 			=> 'plain',
-                'prev_text'		=> $prev_arrow,
-                'next_text'		=> $next_arrow,
-             ));
-        }
-    }
-}
-
-function get_ajax_posts() {
-    
-    $pageRequest = $_POST['page'];
-
-    $args = array(
-        'post_type' => array('kidartists'),
-        'post_status' => array('publish'),
-        'posts_per_page' => 1,
-        'paged' => $pageRequest,
-        'order' => 'DESC',
-        'orderby' => 'date',
-    );
-
-    $ajaxposts = new WP_Query($args);
-    $response = array();
-
-    if ($ajaxposts->have_posts()) {
-        while ($ajaxposts->have_posts()) {
-            $ajaxposts->the_post();
-            $totalPages = $ajaxposts->max_num_pages;
-            $currentPage = $ajaxposts->query['paged'];
-            include(locate_template('template-parts/kid-artists.php', false, false));
         }
     }
 
-    exit;
-}
+    ////////////////////////////////////////
+    // Ajax Posts
+    ////////////////////////////////////////
 
-add_action('wp_ajax_get_ajax_posts', 'get_ajax_posts');
-add_action('wp_ajax_nopriv_get_ajax_posts', 'get_ajax_posts');
+    function get_ajax_posts() {
+        
+        $pageRequest = $_POST['page'];
+
+        $args = array(
+            'post_type' => array('kidartists'),
+            'post_status' => array('publish'),
+            'posts_per_page' => 1,
+            'paged' => $pageRequest,
+            'order' => 'DESC',
+            'orderby' => 'date',
+        );
+
+        $ajaxposts = new WP_Query($args);
+        $response = array();
+
+        if ($ajaxposts->have_posts()) {
+            while ($ajaxposts->have_posts()) {
+                $ajaxposts->the_post();
+                $totalPages = $ajaxposts->max_num_pages;
+                $currentPage = $ajaxposts->query['paged'];
+                include(locate_template('template-parts/kid-artists.php', false, false));
+            }
+        }
+
+        exit;
+    }
+
+    add_action('wp_ajax_get_ajax_posts', 'get_ajax_posts');
+    add_action('wp_ajax_nopriv_get_ajax_posts', 'get_ajax_posts');
