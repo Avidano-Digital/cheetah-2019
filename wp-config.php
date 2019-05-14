d99cca4707297fd168d634ea811d7ae69082c149
Sean Dennis <sean@fullwindsor.co>
2019-05-14 11:20:02 -0500

    Changed news and sp author fields to save on post.

--- wp-config.php
+++ wp-config.php
@@ -48,7 +48,7 @@ define('DB_USER', 'root');
 
 /** MySQL database password */
 
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 
@@ -62,9 +62,9 @@ define('DB_CHARSET', 'utf8mb4');
 
 define('DB_COLLATE', '');
 
-define('WP_SITEURL', 'http://cheetah-2019/');
+// define('WP_SITEURL', 'http://cheetah/');
 
-define('WP_HOME', 'http://cheetah-2019/');
+// define('WP_HOME', 'http://cheetah/');
 
 /**#@+
 
@@ -147,15 +147,16 @@ define('WP_DEBUG', false);
 define('WP_ALLOW_MULTISITE', true);
 
 define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'cheetah-2019');
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'cheetah');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 define('ADMIN_COOKIE_PATH', '/');
 define('COOKIE_DOMAIN', '');
 define('COOKIEPATH', '');
 define('SITECOOKIEPATH', '');
-
-define('WP_MEMORY_LIMIT', '64M');
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -163,7 +164,7 @@ define('WP_MEMORY_LIMIT', '64M');
 
 if ( !defined('ABSPATH') )
 
-	define('ABSPATH', dirname(__FILE__) . '/');
+  define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 
--- wp-content/themes/ccf-twentynineteen/lib/acf-theme-options.php
+++ wp-content/themes/ccf-twentynineteen/lib/acf-theme-options.php
@@ -41,3 +41,53 @@
         endif;
     }
     add_action('acf/save_post', 'change_news_author', 20);
+
+    function change_sp_author( $post_id ) {
+       
+        if ( empty($_POST['acf']) ) :
+            return;
+        endif;
+        
+        // get term id from $post_id
+        $stored_authors = wp_get_post_terms($post_id, 'scientific-paper-author');
+        $resource_category = wp_get_post_terms($post_id, 'resource-category');
+
+        if ($resource_category[0]->name == 'Scientific Papers') :
+
+          // var_dump($stored_authors);
+
+          wp_delete_object_term_relationships( $post_id, 'scientific-paper-author' );
+
+            // foreach ($stored_authors as $stored_author) :
+
+                $stored_author_id = $stored_author->term_id;
+                
+                // get submitted value from acf form
+                $posted_authors = $_POST['acf']['field_5c94d9a961648'];
+
+                // var_dump($posted_authors);
+
+                if ($posted_authors) :
+
+                    foreach ($posted_authors as $posted_author) :
+                    
+                        // get term_id for the submitted value       
+                        $term_id = get_term_by( 'id', $posted_author, 'scientific-paper-author' );
+
+                        var_dump($term_id);
+                        
+                        // if stored value is not equal to posted value, then update terms
+                        if ($stored_author_id != $posted_author) :
+                            // var_dump($posted_author);
+                            wp_set_object_terms( $post_id, (int) $term_id->term_taxonomy_id, 'scientific-paper-author', true );
+                        endif;
+
+                    endforeach;
+
+                endif;
+
+            // endforeach;
+
+        endif;
+    }
+    add_action('acf/save_post', 'change_sp_author', 20);
