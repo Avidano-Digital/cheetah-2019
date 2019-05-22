26b25543111a98cb94b92b424890cec88af14729
Sean Dennis <sean@fullwindsor.co>
2019-05-22 15:22:28 -0500

    Updated search results page.

--- wp-content/themes/ccf-twentynineteen/search.php
+++ wp-content/themes/ccf-twentynineteen/search.php
@@ -36,12 +36,20 @@ get_header(); ?>
     <?php 
 
         if ( have_posts() ) :
+    ?>
+            <ul>
+    <?php          
+                while ( have_posts() ) : the_post();
 
-            while ( have_posts() ) : the_post();
-
-                the_title();
-
-            endwhile;
+    ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </li>
+    <?php
+                endwhile;
+    ?>
+                </ul>
+    <?php
 
         else :
 
