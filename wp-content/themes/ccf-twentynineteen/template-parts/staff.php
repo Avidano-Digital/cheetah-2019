053c648f295ac29260443ee561570dadcc44531c
Carl Avidano <cavidano@gmail.com>
2019-05-24 05:12:37 -0400

    updating staff

--- wp-content/themes/ccf-twentynineteen/template-parts/staff.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/staff.php
@@ -1,16 +1,143 @@
 <div class="narrow my-4">
 
-<h3>Executive Staff</h3>
+    <h2>Executive Staff</h2>
 
-<div class="row align-items-center my-3">
-    <div class="col-4">
-    <img class="rounded-circle" src="https://via.placeholder.com/250x250" alt="Placeholder">
+    <div class="row align-items-center my-3">
+        <div class="col-4">
+            <img class="rounded-circle" src="https://via.placeholder.com/250x250" alt="Placeholder">
+        </div>
+        <!-- .col -->
+        <div class="col-8">
+            <h3 class="h4 mb-0">Laurie L. Marker, DPhil.</h3>
+            <p class="mb-1"><em>Founder, Executive Director</em></p>
+            <p><a href="#">About Dr. Laurie</a></p>
+        </div>
+        <!-- .col -->
     </div>
-    <div class="col-8">
-    <h4>Dr. Laurie Marker</h4>
-    <p>Founder, Executive Director</p>
+    <!-- .row -->
+
+     <div class="row align-items-center my-3">
+        <div class="col-4">
+            <img class="rounded-circle" src="https://via.placeholder.com/250x250" alt="Placeholder">
+        </div>
+        <!-- .col -->
+        <div class="col-8">
+            <h3 class="h4 mb-0">Bruce Brewer, PhD</h3>
+            <p class="mb-1"><em>General Manager</em></p>
+            <p>(1998 – present) and also runs the CCF Bushblok project</p>
+        </div>
+        <!-- .col -->
+    </div>
+    <!-- .row -->
+
+    <div class="row align-items-center my-3">
+        <div class="col-4">
+            <img class="rounded-circle" src="https://via.placeholder.com/250x250" alt="Placeholder">
+        </div>
+        <!-- .col -->
+        <div class="col-8">
+            <h3 class="h4 mb-0">Anne Schmidt-Küntzel</h3>
+            <p class="mb-1"><em>Research Geneticist and Assistant Director for Animal Health and Research</em></p>
+        </div>
+        <!-- .col -->
+    </div>
+    <!-- .row -->
+
+
+
+</div>
+<!-- .narrow -->
+
+<div class="medium my-5">
+
+    <div class="accordion-group my-3" role="tablist" id="accordion-01">
+
+        <div class="card">
+
+            <a class="card-header h4 collapse" id="acc-button-01" data-toggle="collapse" href="#panel-acc-button-01"  role="tab" aria-expanded="true" aria-controls="panel-acc-button-01">
+                <span class="title" role="heading" aria-level="3">Namibian Staff</span>
+            </a>
+
+            <div class="collapse show" id="panel-acc-button-01" role="tabpanel" aria-labelledby="acc-button-01" data-parent="#accordion-01">
+
+                <div class="card-body bg-light overflow-hidden">
+
+                    <div class="narrow my-4">
+
+                        <h3 class="h2 font-weight-light">Genetics</h3>
+                    
+                        <ul class="extensible-list my-2">
+                            <li>
+                                <h4 class="h5 mb-0">Nina Sausgruber, PhD</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Manager</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                            <li>
+                                <h4 class="h5 mb-0">Monika N. Nanghama</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Assistant</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                            <li>
+                                <h4 class="h5 mb-0">Hafeni Hamalwa</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Master Student</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- .narrow -->
+                    <div class="narrow my-4">
+
+                        <h3 class="h2 font-weight-light">Animal Care</h3>
+                    
+                        <ul class="extensible-list my-2">
+                            <li>
+                                <h4 class="h5 mb-0">Nina Sausgruber, PhD</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Manager</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                            <li>
+                                <h4 class="h5 mb-0">Monika N. Nanghama</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Assistant</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                            <li>
+                                <h4 class="h5 mb-0">Hafeni Hamalwa</h5>
+                                <p class="mb-0"><em>Genetics Laboratory Master Student</em></p>
+                                <p class="fs-md text-muted">(2018 – present)</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- .narrow -->
+                </div>
+
+            </div>
+            <!-- .collapse -->
+
+        </div>
+        <!-- .card -->
+
+        <div class="card">
+
+            <a class="card-header h4 collapse collapsed" id="acc-button-01" data-toggle="collapse" href="#panel-acc-button-02"  role="tab" aria-expanded="false" aria-controls="panel-acc-button-02">
+                <span class="title" role="heading" aria-level="3">Accordion Title</span>
+            </a>
+
+            <div class="collapse" id="panel-acc-button-02" role="tabpanel" aria-labelledby="acc-button-01" data-parent="#accordion-01">
+
+                <div class="card-body bg-light">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore debitis provident natus, unde fugit
+                        voluptatibus quia molestias odit deleniti, maxime tempora quod magni sit praesentium ipsam neque
+                        consequatur voluptates.
+                    </p>
+                </div>
+
+            </div>
+            <!-- .collapse -->
+
+        </div>
+
     </div>
 </div>
+<!-- .medium -->
 
-</div>
-<!-- .container -->
