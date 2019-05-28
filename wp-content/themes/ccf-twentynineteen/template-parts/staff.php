0d7166e1a651487e4ae52b3021a2d1b7e99fc93f
Carl Avidano <cavidano@gmail.com>
2019-05-28 09:27:16 -0400

    adding staff list

--- wp-content/themes/ccf-twentynineteen/template-parts/staff.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/staff.php
@@ -66,378 +66,133 @@ $bio = get_sub_field('bio');
 <?php endwhile; endif; /* executive_staff */ ?>
 
 
-<div class="medium my-5">
 
-    <div class="nav nav-pills border device-md" role="tablist">
-
-        <a class="nav-item nav-link flex-fill active" id="tab-btn-01-a" href="#tab-01-a"
-            data-toggle="pill" aria-controls="tab-01-a" aria-selected="true" role="tab">
-            Namibia
-        </a>
-
-        <a class="nav-item nav-link flex-fill" id="tab-btn-01-b" href="#tab-01-b"
-            data-toggle="pill" aria-controls="tab-01-b" aria-selected="false" role="tab">
-            United Kingdom
-        </a>
-
-        <a class="nav-item nav-link flex-fill" id="tab-btn-01-c" href="#tab-01-c"
-            data-toggle="pill" aria-controls="tab-01-c" aria-selected="false" role="tab">
-            United States
-        </a>
-
-    </div>
-
-</div>
-<!-- .medium -->
-
-<div class="tab-content narrow" id="tabs-01-content">
-
-    <div class="tab-pane fade show active" id="tab-01-a" aria-labelledby="tab-btn-01-a" role="tabpanel">
-        
-        <h2 class="display-4 font-weight-light text-muted mb-3">Namibian Staff</h2>
-
-        <div class="my-3">
-
-            <h3 class="h2">Genetics</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li class="mb-2">
-                    <h4>Nina Sausgruber, PhD</h4>
-                    <p><em>Genetics Laboratory Manager</em></p>
-                    <p>2018 – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Monika N. Nanghama</h4>
-                    <p><em>Genetics Laboratory Assistant</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Hafeni Hamalwa</h4>
-                    <p><em>Genetics Laboratory Master Student</em></p>
-                    <p>2017 – Present</p>
-                </li>
-            </ul>
-
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Animal Care</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Lora Allen</h4>
-                    <p><em>Head of Cheetah Department</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Robin Gieling</h4>
-                    <p><em>Veterinarian</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Eli Walker</h4>
-                    <p><em>Curator</em></p>
-                    <p>2013 – Present</p>
-                </li>
-                <li>
-                    <h4>William Versfeld</h4>
-                    <p><em>Laboratory Technichian</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Becky Johnston</h4>
-                    <p><em>Cheetah Keeper and Studbook Assistant</em></p>
-                    <p>2016 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Field (research, ecology and human/wildlife conflict)</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Matti Nghikembua</h4>
-                    <p><em>Senior Ecologist and Forest Steward</em></p>
-                    <p>1997 – Present</p>
-                </li>
-                <li>
-                    <h4>Nadja Le Roux</h4>
-                    <p><em>Community Development Manager</em></p>
-                    <p>2016 – Present</p>
-                </li>
-                <li>
-                    <h4>Willem Daniel Briers-Louw</h4>
-                    <p><em>Ecologist</em></p>
-                    <p>2018 – Present</p>
-                </li>
-                <li>
-                    <h4>Carolina Torres</h4>
-                    <p><em>Ecologist/Conservation Scientist</em></p>
-                    <p>2018 – Present</p>
-                </li>
-                <li>
-                    <h4>Tim Hofmann</h4>
-                    <p><em>Scat Dog Handler, Research Manager and Research Coordinator</em></p>
-                    <p>2018 – Present</p>
-                </li>
-                <li>
-                    <h4>Quentin de Jager</h4>
-                    <p><em>Scat Dog Handler and Trainer</em></p>
-                    <p>2018 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Model Farm</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Farms Manager</h4>
-                    <p><em>Johan Britz</em></p>
-                    <p>2003 – Present</p>
-                </li>
-                <li>
-                    <h4>Paul Visser</h4>
-                    <p><em>Assistant Farms Manager</em></p>
-                    <p>2012 – Present</p>
-                </li>
-                <li>
-                    <h4>Ruan Jacobs</h4>
-                    <p><em>Assistant Estate Manager</em></p>
-                    <p>2017 - present</p>
-                </li>
-                <li>
-                    <h4>Paige Seitz</h4>
-                    <p><em>Livestock Guarding Dog Programme Manager</em></p>
-                    <p>2013 – Present</p>
-                </li>
-                <li>
-                    <h4>Toivo Tyapa</h4>
-                    <p><em>Small Livestock Manager</em></p>
-                    <p>2011 – Present</p>
-                </li>
-                <li>
-                    <h4>Bessie and Max Simon</h4>
-                    <p><em>Farm and Maintenance Supervisors</em></p>
-                    <p>2003 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Hospitality and Tourism</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Hanlie Visser</h4>
-                    <p><em>Hospitality Manager</em></p>
-                    <p>2012 – Present</p>
-                </li>
-                <li>
-                    <h4>Bianca Jacobs</h4>
-                    <p><em>Tourism Manager</em></p>
-                    <p>2013 – Present</p>
-                </li>
-                <li>
-                    <h4>Tryves Shivolo</h4>
-                    <p><em>Tourism Guide</em></p>
-                    <p>2014 – Present</p>
-                </li>
-                <li>
-                    <h4>Job Iyambo</h4>
-                    <p><em>Tourism Guide</em></p>
-                    <p>2010 – Present</p>
-                </li>
-                <li>
-                    <h4>Himeezembi Kuhanga</h4>
-                    <p><em>Tourism Guide</em></p>
-                    <p>2016 – Present</p>
-                </li>
-                <li>
-                    <h4>Kristofina Keendjele</h4>
-                    <p><em>Tourism Support</em></p>
-                    <p>2014 – Present</p>
-                </li>
-                <li>
-                    <h4>Ester Shipanga</h4>
-                    <p><em>Tourism Support</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Meriam Shuudi</h4>
-                    <p><em>Tourism Support</em></p>
-                    <p>2014 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Education</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Annetjie Siyaya</h4>
-                    <p><em>Research and Education Manager</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Gebhardt Nikanor</h4>
-                    <p><em>Education Officer, Livestock Guarding Dog</em></p>
-                    <p>2001 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-        <div class="my-3">
-
-            <h3 class="h2">Administrative</h3>
-        
-            <ul class="extensible-list staff-list my-2">
-                <li>
-                    <h4>Lauren Pfeiffer</h4>
-                    <p><em>Personal Assistant to Dr. Laurie Marker</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Heike Stackmann</h4>
-                    <p><em>Volunteer Coordinator and Public Relations Officer</em></p>
-                    <p>2006 – Present</p>
-                </li>
-                <li>
-                    <h4>Karin Falk</h4>
-                    <p><em>Accountant, CCF</em></p>
-                    <p>2010 – Present</p>
-                </li>
-                <li>
-                    <h4>Tanya Britz</h4>
-                    <p><em>Accountant, CCF Bush</em></p>
-                    <p>2006 – Present</p>
-                </li>
-                <li>
-                    <h4>Anne-Marie Bekker</h4>
-                    <p><em>Business Manager</em></p>
-                    <p>2017 – Present</p>
-                </li>
-                <li>
-                    <h4>Denise Crause</h4>
-                    <p><em>Administrative Assistant</em></p>
-                    <p>2018 – Present</p>
-                </li>
-            </ul>
-        </div>
-        <!-- .narrow -->
-
-    </div>
-    <!-- .tab-pane -->
-
-    <div class="tab-pane fade" id="tab-01-b" aria-labelledby="tab-btn-01-b" role="tabpanel">
-        
-        <h2 class="display-4 font-weight-light text-muted mb-3">United Kingdom Staff</h2>
+<?php if (have_rows('staff_by_country')): ?>
     
-        <div class="my-3">
-        
-            <ul class="extensible-list staff-list my-2">
-                <li class="mb-2">
-                    <h4>Faith Ingham</h4>
-                    <p><em>Head of Fundraising</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Emma Daley</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-            </ul>
-
-        </div>
-        <!-- .narrow -->
-
-    </div>
-    <!-- .tab-pane -->
-
-    <div class="tab-pane fade" id="tab-01-c" aria-labelledby="tab-btn-01-c" role="tabpanel">
-        
-        <h2 class="display-4 font-weight-light text-muted mb-3">United States Staff</h2>
+    <div class="medium my-5">
     
-        <div class="my-3">
+        <div class="nav nav-pills border device-md" role="tablist">
+
+        <?php while (have_rows('staff_by_country')): the_row();
         
-            <ul class="extensible-list staff-list my-2">
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-                <li class="mb-2">
-                    <h4>Full Name</h4>
-                    <p><em>Professional Title</em></p>
-                    <p>20XX – Present</p>
-                </li>
-            </ul>
+        $country = get_sub_field('country');
+        
+        $name = $country['name'];
+
+        $append_id = str_replace(' ', '-', strtolower($name));
+
+        ?>
+
+        <?php if($country): ?>
+
+        <a  class="nav-item nav-link flex-fill <?php if(get_row_index() == 1): ?>active<?php endif; ?>" 
+            id="tab-btn-<?php echo $append_id; ?>" 
+            href="#tab-<?php echo $append_id; ?>"
+            data-toggle="pill" 
+            aria-controls="tab-<?php echo $append_id; ?>" 
+
+            <?php if(get_row_index() == 1): ?>
+            aria-selected="true"
+            <?php else: ?>
+            aria-selected="false"
+            <?php endif; ?>
+
+            role="tab">
+            <?php echo $name; ?> / <?php echo get_row_index(); ?>
+        </a>
+
+        <?php endif; /* country */ ?>
+
+        <?php endwhile; /* staff_by_country */ ?>
 
         </div>
-        <!-- .narrow -->
+        <!-- .nav -->
+        
+	</div>
+    <!-- .medium -->
+
+    <div class="tab-content narrow">
+
+    <?php while ( have_rows('staff_by_country') ): the_row();
+        
+        $country = get_sub_field('country');
+        
+        $name = $country['name'];
+
+        $append_id = str_replace(' ', '-', strtolower($name));
+
+        ?>
+
+        <div    class="tab-pane fade show <?php if (get_row_index() == 1): ?>active<?php endif; ?>"
+                id="tab-<?php echo $append_id; ?>"
+                aria-labelledby="tab-btn<?php echo $append_id; ?>"
+                role="tabpanel">
+        
+            <h2 class="display-4 font-weight-light text-muted mb-3"><?php echo $name; ?> Staff</h2>
+
+
+
+            <?php if (have_rows('staff_list_group')): ?>
+
+            WHOA!!!!!!
+
+            <?php while (have_rows('staff_list_group')): the_row(); ?>
+
+
+                <div class="my-3 bg-danger py-5">
+                
+                    <?php if ($header): ?>
+                    <h3 class="h2">
+                        <?php echo $header; ?>
+                    </h3>
+                    <?php endif; ?>
+
+                    <ul class="extensible-list staff-list my-2">
+
+                        <?php if (have_rows('staff_person')): while (have_rows('staff_person')): the_row();
+
+                        $name = get_sub_field('name');
+                        $title = get_sub_field('title');
+                        $time = get_sub_field('time');
+
+                        ?>
+
+                        <li class="mb-2">
+                            <h4><?php echo $name; ?></h4>
+                            <p><em><?php echo $title; ?></em></p>
+                            <p><?php echo $time; ?></p>
+                        </li>
+
+                         <?php endwhile; endif; /* staff_person */ ?>
+
+                    </ul>
+
+                </div>
+                <!-- .my-3 -->
+
+            <?php endwhile; /* staff_list_group */ ?>
+            <?php endif; /* staff_list_group */ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+        <!-- .tab-pane -->
+
+        <?php endwhile; /* staff_by_country */ ?>
 
     </div>
-    <!-- .tab-pane -->
+    <!-- .tab-content -->
 
-</div>
-<!-- .tab-content -->
+<?php endif; /* staff_by_country */ ?>
+
+
 
