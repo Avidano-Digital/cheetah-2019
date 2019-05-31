9e4f5da65a3a53926524dbff1622d4a37e5b5dfa
Carl Avidano <cavidano@gmail.com>
2019-05-31 06:51:08 -0400

    updated donate stuff

--- wp-content/themes/ccf-twentynineteen/single-resourcelibrary.php
+++ wp-content/themes/ccf-twentynineteen/single-resourcelibrary.php
@@ -143,44 +143,7 @@ $resourceCategory = $resourceCategories[0];
 
     <?php endwhile; endif; /* have_posts */ ?>
 
-    <section class="featured-panel responsive-lg">
-
-      <div class="card bg-dark">
-        <img class="card-img opacity-30" src="<?php echo get_template_directory_uri(); ?>/images/donate.jpg" alt="Placeholder">
-        <div class="card-img-overlay d-flex px-0">
-
-          <div class="container align-self-center">
-
-            <div class="narrow py-4">
-
-              <h5 class="card-title h1 text-white text-center text-shadow">
-                Join the race to
-                <a class="text-primary" href="#">#SaveTheCheetah</a>
-              </h5>
-
-              <div class="row matrix-border shadow-lg">
-
-                <div class="col-md-6">
-                  <a class="btn btn-lg btn-block btn-primary" href="/donate" title="Donate">Donate</a>
-                </div>
-                <!-- .col -->
-
-                <div class="col-md-6">
-                  <a class="btn btn-lg btn-block btn-primary" href="#" title="Sponsor a Cheetah">Sponsor a Cheetah</a>
-                </div>
-                <!-- .col -->
-
-              </div>
-              <!-- .matrix-border -->
-
-            </div>
-
-          </div>
-          <!-- .container -->
-        </div>
-      </div>
-
-    </section>
+    <?php get_template_part('template-parts/donate-panel'); ?>
 
 </main>
 <!-- #content -->
