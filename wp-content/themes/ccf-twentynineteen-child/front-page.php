37e47827d564c7ec5ca7d2877a4f71af001b4d55
Carl Avidano <cavidano@gmail.com>
2019-05-31 05:20:09 -0400

    before fixing  events

--- wp-content/themes/ccf-twentynineteen-child/front-page.php
+++ wp-content/themes/ccf-twentynineteen-child/front-page.php
@@ -30,26 +30,18 @@
           <div class="card-img-overlay d-flex">
             <div class="align-self-center">
 
-            <div class="container text-white">
+              <div class="container text-white">
 
-            <div class="narrow-max-width">
+                <div class="narrow-max-width">
+                  <h1 class="display-3 mb-2  text-shadow">Devoted to Saving Cheetahs in the Wild</h1>
+                  <a href="#" class="btn btn-lg btn-primary">About Us</a>
+                </div>
 
-                <h1 class="display-3 mb-2  text-shadow">Devoted to Saving Cheetahs in the Wild</h1>
-                <a href="#" class="btn btn-lg btn-primary">About Us</a>
-            </div>
-
-
-            </div>
-            <!-- .comtainer -->
-
-
-
-
-              <div class="narrow text-white text-center my-lg-3 d-none">
-                <h1 class="display-2 mb-2 text-shadow">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
-                <a href="#" class="btn btn-lg btn-primary">About Us</a>
               </div>
+              <!-- .comtainer -->
+
             </div>
+            <!-- .align-self-center -->
           </div>
 
         </div>
@@ -61,7 +53,43 @@
     </section>
     <!-- #introduction -->
 
-    <section class="bg-light pattern-border-top-bottom py-6">
+    <section class="bg-light py-6 d-none">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg mb-3 mb-lg-0">
+            <h2 class="h1">Lorem ipsum dolor sit amet consectetur elit</h2>
+            <a class="btn btn-lg btn-primary mt-2 d-none d-lg-inline-block" href="#" aria-hidden="true"
+              title="Button Link">Button Link</a>
+          </div>
+
+          <div class="col-lg">
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore voluptas quod magni saepe. Expedita ab
+              vitae quo, dolore modi nesciunt deserunt voluptatibus? Quidem ducimus est non quisquam exercitationem.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima id quibusdam molestiae, et nisi
+              nostrum exercitationem consectetur perferendis earum.
+            </p>
+
+            <a href="#" class="btn btn-lg btn-primary mt-2 d-lg-none" title="Button Link">Button Link</a>
+          </div>
+          <!-- .col -->
+
+        </div>
+        <!-- .row -->
+
+      </div>
+      <!-- .container -->
+
+    </section>
+
+    <section class="bg-light pattern-border-top-bottom py-6 d-none">
 
       <div class="container">
 
@@ -106,26 +134,24 @@
 
     </section>
 
-    <!-- #introduction -->
-
     <section class="py-5 py-xl-7">
 
-      <h2 class="sr-only">News and Videos from CCF</h2>
+      <h2 class="sr-only">News and Events</h2>
 
       <section class="container mb-5" id="latest-news">
 
-        <header class="text-center mb-4">
-          <h3 class="display-4 mb-0">Latest News</h3>
-          <a class="blended-link fs-md" href="#">All News</a>
+        <header class="text-center mb-3">
+            <h3 class="display-4 mb-0">Latest News</h3>
+            <a class="link text-body fs-md" href="#">All News</a>
         </header>
 
-        <div class="row matrix-border">
+        <div class="row matrix-gutter">
 
           <?php
 
           global $post;
           
-          $args = array( 'category_name' => 'conservation', 'posts_per_page' => 3, 'orderby' => 'date' );
+          $args = array( 'posts_per_page' => 3, 'orderby' => 'date' );
           $postslist = get_posts( $args );
           
           foreach ( $postslist as $post ) :
@@ -173,83 +199,102 @@
       </section>
       <!-- #latest-news -->
 
-            <?php
+      <?php
 
-            $args = array( 
-                'post_type' => 'events', 
-                'posts_per_page' => '3',
-                'order' => 'ASC',
-                'meta_key' => 'start_date',
-                'meta_type' => 'DATETIME',
-                'orderby' => 'meta_value',
-            );
+      $args = array( 
+          'post_type' => 'events', 
+          'posts_per_page' => '3',
+          'order' => 'ASC',
+          'meta_key' => 'start_date',
+          'meta_type' => 'DATETIME',
+          'orderby' => 'meta_value',
+      );
 
-            $events_header = false;
-            
-            $loop = new WP_Query( $args );
+      $events_header = false;
+      
+      $loop = new WP_Query( $args );
 
-                while ( $loop->have_posts() ) : $loop->the_post();
+      $count = $wp_query->post_count;
 
-                    $date = new DateTime(get_field('start_date'));
-                    $today = DateTime::createFromFormat("U", time());
+      
+      while ( $loop->have_posts() ) : $loop->the_post();
+        
+      $date = new DateTime(get_field('start_date'));
+      $today = DateTime::createFromFormat("U", time());
+      
+      if ($date > $today) :
+      if ($events_header == false) : ?>
+      
+      <section class="container" id="events">
 
-                    if ($date > $today) :
-                        if ($events_header == false) :
-            ?>
+        <header class="text-center mb-3">
+            <h3 class="display-4 mb-0">Upcoming Events / <?php echo $count; ?></h3>
+            <a class="link text-body fs-md" href="#">All Events</a>
+        </header>
 
-                            <section class="container-fluid wide" id="events">
+        <div class="row matrix-border">
 
-                              <header class="text-center mb-4">
-                                  <h3 class="display-4 mb-0">Upcoming Events</h3>
-                                  <a class="blended-link fs-md" href="#">All Events</a>
-                              </header>
+          <?php endif;
 
-                              <div class="row matrix-border">
+          $featured_image_id = get_post_thumbnail_id($post->ID);
+          $featured_image = wp_get_attachment_image_src($featured_image_id,'full', false, '');
+          $featured_image_alt = get_post_meta($featured_image_id,'_wp_attachment_image_alt', true);
 
-                        <?php
-
-                        endif;
-
-                        $featured_image_id = get_post_thumbnail_id($post->ID);
-                        $featured_image = wp_get_attachment_image_src($featured_image_id,'full', false, '');
-                        $featured_image_alt = get_post_meta($featured_image_id,'_wp_attachment_image_alt', true);
-
-                        ?>
-
-                        <div class="col-md-6 d-md-flex mb-3 mb-md-0 col-xl-4 offset-xl-2">
-                          <div class="card bg-light">
-                            <div class="card-header bg-info text-white text-center font-weight-bold py-2">
-                              <?php echo $date->format('M j, Y'); ?>
-                            </div>
-                            <?php if ($featured_image) : ?>
-                                <img class="card-img-top" src="<?php echo $featured_image[0]; ?>" alt="<?php echo $featured_image_alt; ?>">
-                            <?php else : ?>
-                                <img class="card-img" src="https://via.placeholder.com/1000x563" alt="Placeholder">
-                            <?php endif; ?>
-                            <div class="card-body">
-                              <h4 class="card-title mb-2"><?php the_title(); ?></h4>
-                            </div>
-                            <div class="card-footer pt-0">
-                              <a class="btn btn-primary btn-block" href="<?php the_permalink(); ?>">Event Details</a>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- .col -->
-                        <?php if ( $events_header == false ) : ?>
-                            </div>
-                            <!-- .matrix-border -->
-
-                        </section>
-                        <!-- #videos -->
-                        <?php
-                        
-                          $events_header = true;
-
-                      endif;
-                  endif;
-                  
-              endwhile;
           ?>
+
+          <div class="col-md-4 <?php if ($count == 1): ?>offset-md-4 <?php endif; ?> <?php if ($count == 2): ?>offset-md-2 <?php endif; ?>">
+
+            <div class="card border h-100">
+
+              <?php if ($featured_image) : ?>
+              <img class="card-img-top" src="<?php echo $featured_image[0]; ?>"
+                alt="<?php echo $featured_image_alt; ?>">
+              <?php else : ?>
+              <img class="card-img" src="https://via.placeholder.com/1000x563" alt="Placeholder">
+              <?php endif; ?>
+
+              <div class="card-body">
+
+                <p class="has-icon f-sans-serif fs-md mb-1">
+                  <span class="fas fa-map-marker-alt"></span>
+                  <span class="title"><?php the_field('location') ?></span>
+                </p>
+
+                <h2 class="h5"><?php the_title(); ?></h2>
+
+                <p class="f-sans-serif fs-md">
+
+                  <strong class="d-block"><?php the_field('start_date') ?></strong>
+
+                  <?php if ($time) : ?>
+                  <span class="text-muted"><?php echo $time; ?></span>
+                  <?php endif; ?>
+                </p>
+              </div>
+              <!-- .card-body -->
+
+              <div class="card-footer py-2">
+                <a href="<?php the_permalink(); ?>" class="btn btn-block btn-primary stretched-link">
+                  Event Details
+                </a>
+              </div>
+              <!-- .card-footer -->
+
+            </div>
+            <!-- .card -->
+
+          </div>
+          <!-- .col -->
+
+          <?php if ( $events_header == false ) : ?>
+
+        </div>
+        <!-- .matrix-border -->
+
+      </section>
+      <!-- #videos -->
+
+      <?php $events_header = true; endif; endif; endwhile; ?>
 
     </section>
 
