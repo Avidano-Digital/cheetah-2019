6ac1f6af938d9e86c19a2b9c1dd68f2b9c48db35
Sean Dennis <sean@fullwindsor.co>
2019-06-03 14:56:02 -0500

    Added social sharing functionality.

--- wp-content/themes/ccf-twentynineteen/template-parts/article-footer.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/article-footer.php
@@ -10,19 +10,21 @@
 
             <ul class="extensible-list horizontal justify-content-center">
 
+              <?php global $wp; ?>
+
                 <li>
-                    <a href="#" title="Share on Facebook">
+                    <a href="http://www.facebook.com/sharer.php?u=<?php echo home_url($wp->request) ?>&t=Cheetah Conservation Fund" target="_blank" title="Share on Facebook">
                         <img class="rounded-circle" src="<?php echo get_template_directory_uri(); ?>/images/share-facebook.svg" alt="Share">
                     </a>
                 </li>
 
                 <li>
-                    <a href="#" title="Share on Twitter">
+                    <a href="http://twitter.com/share?text=Cheetah Conservation Fund&url=<?php echo home_url($wp->request) ?>" target="_blank" title="Share on Twitter">
                         <img class="rounded-circle" src="<?php echo get_template_directory_uri(); ?>/images/share-twitter.svg" alt="Share">
                     </a>
                 </li>
                 <li>
-                    <a href="#" title="Email a friend">
+                    <a href="mailto:?subject=Cheetah Conservation Fund&body=Check out <?php echo home_url($wp->request) ?>" target="_blank" title="Email a friend">
                         <img class="rounded-circle" src="<?php echo get_template_directory_uri(); ?>/images/share-email.svg" alt="Share">
                         </span>
                     </a>                
